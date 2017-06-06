0651770f98a08bc644d98f3791099069e9970f72
Nicole <nmarielockwood@gmail.com>
2017-06-06 17:52:17 -0400

    updating files

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ app.set("view engine", "handlebars");
 /*******************************************/
 // CONFIGURING DB
 // Database configuration with mongoose
+//mongoose.connect("mongodb://localhost/articles_db");
 mongoose.connect("mongodb://heroku_96srcnwp:k9vqebtelagn88jf1lbiba95c@ds155091.mlab.com:55091/heroku_96srcnwp");
 var db = mongoose.connection;
 
@@ -55,7 +56,7 @@ db.on("error", function(error) {
   console.log("Mongoose Error: ", error);
 });
 
-// Once logged in to the db through mongoose, log a success message
+// Once logged in to the db through mongoose, logging a success message
 db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
@@ -67,7 +68,7 @@ db.once("open", function() {
 app.get("/scrape", function(req, res) {
   // Grabbing the body of the html with request
   request("https://www.nytimes.com/", function(error, response, html) {
-    // Loading that into cheerio and save it to $ for a shorthand selector
+    // Loading that into cheerio and saving it to $ for a shorthand selector
     var $ = cheerio.load(html);
     // Grabbing every h2 within an article tag
     $("article h2").each(function(i, element) {
@@ -75,21 +76,20 @@ app.get("/scrape", function(req, res) {
       // Saving an empty result object
       var result = {};
 
-      // Adding the text and href of every link, and save them as properties of the result object
+      // Adding the text and href of every link, and saving them as properties of the result object
       result.title = $(this).children("a").text().trim();
       result.link = $(this).children("a").attr("href");
 
       // Using the Article model, creating a new entry
-      // This effectively passes the result object to the entry (and the title and link)
       var entry = new Article(result);
 
-      // Now, save that entry to the db
+      // Saving the entry to the db
       entry.save(function(err, doc) {
-        // Log any errors
+        // Logging any errors
         if (err) {
           console.log(err);
         }
-        // Or log the doc
+        // Logging the doc
         else {
           console.log(doc);
         }
@@ -101,15 +101,15 @@ app.get("/scrape", function(req, res) {
   res.redirect("/");
 });
 
-// This will get the articles we scraped from the mongoDB
+// Using a GET request to pull the articles we scraped from the mongoDB
 app.get("/", function(req, res) {
-  // Grab every doc in the Articles array
+  // Grabbing every doc in the Articles array
   Article.find({}, function(error, doc) {
-    // Log any errors
+    // Logging any errors
     if (error) {
       console.log(error);
     }
-    // Or send the doc to the browser
+    // Sending the doc to the browser
     else {
       var articleObj = {
         article: doc
@@ -118,26 +118,28 @@ app.get("/", function(req, res) {
     }
   }).
   sort({ title: 1 }).
-  limit(15);
+  limit(20);
 });
 
+// Using a PUT request to save articles
 app.put("/:id", function(req, res) {
-  // Use the article id to find and update it's status to "saved"
+  // Using the article id to find and update its status to "saved"
   Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": req.body.saved })
-  // Execute the above query
+  // Executing the above query
     .exec(function(err, doc) {
-      // Log any errors
+      // Logging any errors
       if (err) {
         console.log(err);
       }
       else {
-        // Or send the document to the browser
+        // Sending the doc to the browser
         console.log(doc);
       }
     });
     res.redirect("/");
 });
 
+// Using a GET request to pull the saved articles
 app.get("/saved", function(req, res) {
   // Grab every doc in the Articles array that is saved
   Article.find({ saved: true }, function(error, doc) {
@@ -155,8 +157,9 @@ app.get("/saved", function(req, res) {
   });
 });
 
+// Using a PUT request to "unsave" an article
 app.put("/delete/:id", function(req, res) {
-  // Delete an article based on it's ObjectId
+  // Delete an article based on its ObjectId
   Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": req.body.saved })
   // Execute the above query
     .exec(function(err, doc) {
@@ -172,7 +175,25 @@ app.put("/delete/:id", function(req, res) {
   res.redirect("/saved");
 });
 
-// Grab an article by it's ObjectId
+// Trying to use a GET request to pull all saved notes associated with an article
+/*app.get("/saved/:id", function(req, res) {
+  // Grabbing every doc in the Notes array that is saved
+  Note.find({ saved: true }, function(error, doc) {
+    // Logging any errors
+    if (error) {
+      console.log(error);
+    }
+    // Sending the doc to the browser
+    else {
+      var articleObj = {
+        article: doc
+      };
+      res.render("saved", articleObj);
+    }
+  });
+});*/
+
+// Trying to use a GET request to pull all saved notes associated with an article 
 app.get("/saved/:id", function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   Article.findOne({ "_id": req.params.id })
@@ -191,29 +212,27 @@ app.get("/saved/:id", function(req, res) {
   });
 });
 
-// Create a new note or replace an existing note
+// Using a POST request to create a new note or replace an existing note
 app.post("/saved/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
+  // Creating a new note and pass the req.body to the entry
   var newNote = new Note(req.body);
-
-  // And save the new note the db
+  // Saving the new note the db
   newNote.save(function(error, doc) {
-    // Log any errors
+    // Logging any errors
     if (error) {
       console.log(error);
     }
-    // Otherwise
     else {
-      // Use the article id to find and update it's note
+      // Using the article id to find and update its note
       Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
-      // Execute the above query
+      // Executing the above query
       .exec(function(err, doc) {
-        // Log any errors
+        // Logging any errors
         if (err) {
           console.log(err);
         }
         else {
-          // Or send the document to the browser
+          // Sending the doc to the browser
           res.send(doc);
         }
       });
@@ -221,24 +240,25 @@ app.post("/saved/:id", function(req, res) {
   });
 });
 
-app.get("/saved/:id", function(req, res) {
-  // Grab every doc in the Articles array that is saved
-  Note.find({ saved: true }, function(error, doc) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    // Or send the doc to the browser
-    else {
-      var articleObj = {
-        article: doc
-      };
-      res.render("saved", articleObj);
-    }
-  });
+// Trying to use a PUT request to delete saved notes associated with an article 
+app.put("/note/:id", function(req, res) {
+  // Deleting a note based on its ObjectId
+  Note.remove({ "_id": req.params.id })
+  // Executing the above query
+    .exec(function(err, doc) {
+      // Logging any errors
+      if (err) {
+        console.log(err);
+      }
+      else {
+        // Sending the doc to the browser
+        console.log(doc);
+      }
+    });
+  res.redirect("/saved");
 });
 
-// Listen on port 3000
+// Listening on port 3000
 app.listen(PORT, function() {
   console.log("App running on port " + PORT);
 });
